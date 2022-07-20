94f5eb3163e62577465a57db01d5cd4895ffac3f
prog-apprentice401 <koustubhsrivastava1@gmail.com>
2022-07-20 23:49:14 +0530

    ok, so the previous automatic parking didnt work, this one does

--- automaticParking/automaticParking.ino
+++ automaticParking/automaticParking.ino
@@ -3,19 +3,24 @@
 #define TRIG_PIN	2
 #define ECHO_PIN	3
 #define SERVO_PIN	9
+#define DELAY_BEFORE_CLOSE	3000
 
 unsigned long millisAtGatePassing;
 int cm;
 bool isGateOpen = false;
 bool isCarAtGate = false;
 
-servo gateServo;
+Servo gateServo;
 
+int getDuration (int, int);
+void gateClose (Servo gateServo);
+void gateOpen (Servo gateServo);
 
 void setup ()
 {
+	Serial.begin (9600);
 	pinMode (TRIG_PIN, OUTPUT);
-	pinMode (ECHOPIN, INPUT);
+	pinMode (ECHO_PIN, INPUT);
 	gateServo.attach (SERVO_PIN);
 
 	gateClose (gateServo);
@@ -24,13 +29,16 @@ void setup ()
 void loop ()
 {
 	cm = getDistance (TRIG_PIN, ECHO_PIN);
-	if (!isGateOpen && cm <= 15) {
-		gateOpen (gateServo);
-		isGateOpen = true;
-		isCarAtGate = true;
-	} else if (isGateOpen && cm > 15) {
-		//the car has gone just now, hence, we will update car status
-		if (isCarAtGate) {
+
+	if (cm <= 15) {
+		if (!isGateOpen) {
+			gateOpen (gateServo);
+			isGateOpen= true;
+			isCarAtGate = true;
+		}
+	} else if (isGateOpen) {
+		if (isCarAtGate) {	//the car just passed, hence the record is not updated yet
+			Serial.println ("Car gone, timing");
 			millisAtGatePassing = millis ();
 			isCarAtGate = false;
 		} else if (millis () - millisAtGatePassing >= DELAY_BEFORE_CLOSE) {
@@ -49,20 +57,19 @@ int getDistance (int trigPin, int echoPin)
 	digitalWrite (trigPin, HIGH);
 	delayMicroseconds (10);
 	digitalWrite (trigPin, LOW);
-
-	duration = pulsein (echoPin, HIGH);
-	cm = duration/58.82l
+	duration = pulseIn (echoPin, HIGH);
+	cm = duration/58.82l;
 
 	return cm;
 }
 
-void gateOpen (servo gateServo)
+void gateOpen (Servo gateServo)
 {
 	gateServo.write (0);
 	delay (500);
 }
 
-void gateClose (servo gateServo)
+void gateClose (Servo gateServo)
 {
 	gateServo.write (90);
 	delay (500);
